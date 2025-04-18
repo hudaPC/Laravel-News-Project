b627f8fe6dadf29ca0217aa7116073f36f523754
Bodrul Huda <hudactgbd@gmail.com>
2025-04-18 21:41:59 +0600

    sad

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(){
+        // logic....
+        return view('home');
+
+    }
+    public function about_us(){
+        //
+        return ("This is about us from main controller");
+
+    }
+    public function contact_us(){
+        //
+        return ("This is contact us from main controller");
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[MainController::class,'index']);
 
+Route::get('about-us',[MainController::class,'about_us']);
 
-
-
-
-Route::get('about-us',function(){
-    return route('contact');    
-})->name('about');
-
-
-Route::get('news-details/{id}',function($id){
-    return 'News details Post ID => '.$id;    
-});
-
-
-
-
-Route::get('contact-us',function(){
-    // code here
-       // echo 'This is about us page';
-    return 'This is  contact us page';    
-})->name('contact');
-    
+Route::get('contact-us',[MainController::class,'contact_us']);
