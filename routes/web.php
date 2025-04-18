97a261944b8287b9a6d1136b6570dce856409ee9
Bodrul Huda <hudactgbd@gmail.com>
2025-04-18 15:40:30 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+Route::get('about-us',function(){
+    return route('contact');    
+})->name('about');
+
+
+Route::get('news-details/{id}',function($id){
+    return 'News details Post ID => '.$id;    
+});
+
+
+
+
+Route::get('contact-us',function(){
+    // code here
+       // echo 'This is about us page';
+    return 'This is  contact us page';    
+})->name('contact');
+    
